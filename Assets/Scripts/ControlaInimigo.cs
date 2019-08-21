e306906346dc7df0d76758ad2f7b3e36abc366ac
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2019-08-21 00:49:41 -0300

    initial commit

---
+++ Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour {
+
+    public GameObject jogador;
+    public float Velocidade = 5;
+    Vector3 direcao;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate(){
+
+        Vector3 direcao = jogador.transform.position - transform.position;
+
+        GetComponent<Rigidbody>().MovePosition(
+            GetComponent<Rigidbody>().position +
+            direcao.normalized * Velocidade * Time.deltaTime);
+    }
+}
